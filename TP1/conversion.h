05e6b11a4dbae7311b7e00993146f3a7042d437c
AgustinHerzkovich <aherzkovich@frba.utn.edu.ar>
2024-06-17 21:14:57 -0300

    Guardas de inclusión

--- TP1/conversion.h
+++ TP1/conversion.h
@@ -1,1 +1,6 @@
+#ifndef CONVERSION_H_INCLUDED
+#define CONVERSION_H_INCLUDED
+
 float convertirNudosKmH(float);
+
+#endif // CONVERSION_H_INCLUDED
--- TP1/tabla.h
+++ TP1/tabla.h
@@ -1,4 +1,9 @@
+#ifndef TABLA_H_INCLUDED
+#define TABLA_H_INCLUDED
+
 void mostrarTabla(float, float, float);
 void mostrarConFormato(float, float);
 void mostrarInicio();
 int cantidadFilas(float, float, float);
+
+#endif // TABLA_H_INCLUDED
